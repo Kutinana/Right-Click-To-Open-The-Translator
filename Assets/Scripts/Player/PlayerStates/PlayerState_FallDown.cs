22cd30b9f0630432ddab201c8d232a273864e4e4
Yoshino <3118389253@qq.com>
2024-05-06 19:52:19 +0800

    Infinitely times slip down

--- Assets/Scripts/Player/PlayerStates/PlayerState_FallDown.cs
+++ Assets/Scripts/Player/PlayerStates/PlayerState_FallDown.cs
@@ -5,17 +5,25 @@ using UnityEngine.InputSystem;
 [CreateAssetMenu(menuName = "Data/PlayerState/FallDown", fileName = "PlayerState_FallDown")]
 public class PlayerState_FallDown : PlayerState
 {
-
+    [SerializeField] float deceleration = 10f;
     private float LerpParameter;
 
     protected override void StateInitialization()
     {
+        InitialSpeed = playerController.moveSpeed;
+        LerpParameter = 0;
         base.StateInitialization();
     }
     public override void Enter()
     {
         StateInitialization();
+        playerInput.DisableInputActions();
         base.Enter();
+    }
+    public override void Exit()
+    {
+        playerInput.EnableInputActions();
+        base.Exit();
     }
     public override void LogicUpdate()
     {
@@ -23,10 +31,13 @@ public class PlayerState_FallDown : PlayerState
         {
             stateMachine.SwitchState(typeof(PlayerState_Idle));
         }
+        currentSpeed = Mathf.Lerp(InitialSpeed, 0, LerpParameter);
+        LerpParameter = LerpParameter <= 1 ? LerpParameter + deceleration * Time.deltaTime : 1;
         base.LogicUpdate();
     }
     public override void PhysicsUpdate()
     {
+        playerController.MoveWithoutPlayerInput(currentSpeed);
         base.PhysicsUpdate();
     }
 }

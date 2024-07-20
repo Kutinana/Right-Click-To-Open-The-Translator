f8ffe935772a8f3f13eaf52caf6b013088b8e2a9
Kuchinashi Hoshikawa <kuchinashi@ahympia.com>
2024-07-20 15:01:10 +0900

    Change Version

--- Assets/Scripts/StartScene/StartSceneController.cs
+++ Assets/Scripts/StartScene/StartSceneController.cs
@@ -18,7 +18,7 @@ namespace StartScene
 {
     public class StartSceneController : MonoSingleton<StartSceneController>
     {
-        public static string Version => "0.2.2";
+        public static string Version => "0.2.3";
 
         private CanvasGroup mFirstSplashCanvasGroup;
         private CanvasGroup mSecondSplashCanvasGroup;

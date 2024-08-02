1239627bd54193ffb14cbb347c558cf8806b3cb0
Kuchinashi Hoshikawa <kuchinashi@ahympia.com>
2024-08-02 11:13:01 +0800

    Minor Updates

--- Assets/Scripts/Interaction/SpecificObjects/InteractiveDoor.cs
+++ Assets/Scripts/Interaction/SpecificObjects/InteractiveDoor.cs
@@ -96,7 +96,7 @@ public class InteractiveDoor: InteractiveObject
         
         player.GetComponent<PlayerController>().EnableGroundCheck = false;
         player.GetComponent<Rigidbody2D>().simulated = false;
-        player.GetComponent<PlayerInput>().DisableInputActions();
+        // player.GetComponent<PlayerInput>().DisableInputActions();
 
         yield return new WaitForSeconds(0.3f);
 
